fc318e339593f6428b7ea566d5bc14722a716f06
myc1986 <moratinos.c@gmail.com>
2015-01-12 00:12:48 +0100

    ajout de commentaire

--- Client/API_FTP/FTP_Client/Classes/Status.cs
+++ Client/API_FTP/FTP_Client/Classes/Status.cs
@@ -12,13 +12,24 @@ namespace API_FTP.FTP_Client.Classes
     /// </summary>
     abstract class Status : IStatus
     {
+        /// <summary>
+        /// Renseigne sur le commentaire du statut
+        /// </summary>
         protected string _comment;
 
+        /// <summary>
+        /// Obtient le code statut converti en int
+        /// </summary>
+        /// <returns>int</returns>
         public int GetStatusCode()
         {
             return 0;
         }
 
+        /// <summary>
+        /// Obtient le statut détaillé sous forme de chaine de texte
+        /// </summary>
+        /// <returns>string</returns>
         public string GetStatusDetail()
         {
             return "";
