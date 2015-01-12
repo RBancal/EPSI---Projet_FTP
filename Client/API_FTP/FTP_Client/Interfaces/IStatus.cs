a642d5645f8d35194841d311d739e84e1d849a03
myc1986 <moratinos.c@gmail.com>
2015-01-12 20:33:03 +0100

    Ajout de commentaire pour l'interface IStatus

--- Client/API_FTP/FTP_Client/Interfaces/IStatus.cs
+++ Client/API_FTP/FTP_Client/Interfaces/IStatus.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 
 namespace API_FTP.FTP_Client.Interfaces
 {
+    /// <summary>
+    /// Interface des objets fournissant des status
+    /// </summary>
     interface IStatus
     {
+        /// <summary>
+        /// Obtient le code statut
+        /// </summary>
+        /// <returns>nombre entier</returns>
         int GetStatusCode();
+
+        /// <summary>
+        /// Obtient le commentaire du statut
+        /// </summary>
+        /// <returns>chaine (string)</returns>
         string GetStatusDetail();
     }
 }
