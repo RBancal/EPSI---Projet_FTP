412af156fb9b9737f68c172bc2698917f26f2c3b
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:25:32 +0100

    Création de la classe fournissant les méthodes globales pour le client ftp

---
+++ Client/API_FTP/FTP_Client/MethodesGlobales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace API_FTP.FTP_Client
+{
+    public static abstract class MethodesGlobales
+    {
+        public static void AfficherMessager(string message, string titre)
+        {
+            MessageBox.Show(message, titre);
+        }
+
+        public static void AfficherMessageErreurInitialisationILog()
+        {
+            AfficherMessager(VariablesGlobales.GetContenuMessageErreurInitialisationILog, VariablesGlobales.GetTitreMessageErreurInitialisationILog);
+        }
+    }
+}
