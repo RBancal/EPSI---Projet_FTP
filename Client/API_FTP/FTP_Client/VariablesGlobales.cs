b8c8b23a0e2ad7a9e19a40d4ea8ea9af1e44407a
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:46:58 +0100

    Facorisation du message pour l'utiliser sur ILog et ILogable

--- Client/API_FTP/FTP_Client/VariablesGlobales.cs
+++ Client/API_FTP/FTP_Client/VariablesGlobales.cs
@@ -8,10 +8,12 @@ namespace API_FTP.FTP_Client
 {
     static abstract class VariablesGlobales
     {
-        protected const string ERREUR_INITIALISATION_MESSAGE = "Ce logueur n'est pas initialisé. Merci de l'initialisé";
-        protected const string ERREUR_INITIALISATION_TITRE = "Erreur - Logueur";
+        protected const string ERREUR_INITIALISATION_MESSAGE = "Ce {0} N'est pas initialisé. Merci de l'initialisé";
+        protected const string ERREUR_INITIALISATION_TITRE = "Erreur - {0}";
 
-        public static string GetContenuMessageErreurInitialisationILog { get { return ERREUR_INITIALISATION_MESSAGE; } }
-        public static string GetTitreMessageErreurInitialisationILog { get { return ERREUR_INITIALISATION_TITRE;  } }
+        public static string GetContenuMessageErreurInitialisationILog { get { return string.Format(ERREUR_INITIALISATION_MESSAGE, "ILog"); } }
+        public static string GetTitreMessageErreurInitialisationILog { get { return string.Format(ERREUR_INITIALISATION_TITRE, "ILog");  } }
+        public static string GetContenuMessageErreurInitialisationILogable { get { return string.Format(ERREUR_INITIALISATION_MESSAGE, "ILogable"); } }
+        public static string GetTitreMessageErreurInitialisationILogable { get { return string.Format(ERREUR_INITIALISATION_TITRE, "ILogable"); } }
     }
 }
