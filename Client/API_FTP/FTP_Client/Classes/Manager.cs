2b3cfee5bee45ada060c1f8ede73a030366cfb0b
myc1986 <moratinos.c@gmail.com>
2015-01-18 18:22:56 +0100

    mise à jour

--- Client/API_FTP/FTP_Client/Classes/Manager.cs
+++ Client/API_FTP/FTP_Client/Classes/Manager.cs
@@ -41,6 +41,14 @@ namespace API_FTP.FTP_Client.Classes
         abstract public List<ITransfer> ListerContenu(ITransfer leDossier);
 
         /// <summary>
+        /// Liste le contenu d'un dossier
+        /// </summary>
+        /// <param name="leDossier">le dossier à lister</param>
+        /// <param name="avecSousRepertoire">true pour les sous répertoire et false pour ne pas lister les éléments des sous-répertoires. Par défaut ne liste pas les éléments des sous-répertoires</param>
+        /// <returns>retourn la liste des ITranfert contenu dans ce dossier</returns>
+        abstract public List<ITransfer> ListerContenu(ITransfer leDossier, bool avecSousRepertoire = false);
+
+        /// <summary>
         /// Charge le lILog du manager et vérifie si le ILog est ibnitialisé
         /// </summary>
         /// <param name="unLogueur"> le ILog</param>
