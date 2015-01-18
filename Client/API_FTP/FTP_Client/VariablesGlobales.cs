daeabe1bdde17903b90b11cd8d4abc6b1bb9c698
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:26:12 +0100

    Création d'une classe fournissant un accès à des variables globales pour client_ftp

---
+++ Client/API_FTP/FTP_Client/VariablesGlobales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_FTP.FTP_Client
+{
+    static abstract class VariablesGlobales
+    {
+        protected const string ERREUR_INITIALISATION_MESSAGE = "Ce logueur n'est pas initialisé. Merci de l'initialisé";
+        protected const string ERREUR_INITIALISATION_TITRE = "Erreur - Logueur";
+
+        public static string GetContenuMessageErreurInitialisationILog { get { return ERREUR_INITIALISATION_MESSAGE; } }
+        public static string GetTitreMessageErreurInitialisationILog { get { return ERREUR_INITIALISATION_TITRE;  } }
+    }
+}
