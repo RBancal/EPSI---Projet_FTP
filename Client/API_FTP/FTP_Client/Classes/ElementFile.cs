6f4f6bbd4d6786754e717432b81b4d4b4a44bedc
myc1986 <moratinos.c@gmail.com>
2015-01-18 17:05:43 +0100

    Ajout de la classe ElementFile

---
+++ Client/API_FTP/FTP_Client/Classes/ElementFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_FTP.FTP_Client.Classes
+{
+    class ElementFile : Element
+    {
+        public bool EstUnFichier()
+        {
+            return true;
+        }
+
+        public new bool EstUnDossier()
+        {
+            return false;
+        }
+
+        public new long GetSize()
+        {
+            return new FileInfo(Path).Length;
+        }
+
+        public ElementFile(string chemin)
+        {
+            if (!File.Exists(chemin))
+            {
+                throw new FileNotFoundException(string.Format("{0} n'existe pas !", chemin));
+            }
+            else
+            {
+                _path = chemin;
+                Name = new FileInfo(chemin).Name;
+            }
+        }
+    }
+}
