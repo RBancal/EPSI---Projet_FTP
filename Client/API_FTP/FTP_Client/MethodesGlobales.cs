9a97c719a9deae0f2ef0587ecac83e9d6168996d
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:47:55 +0100

    Ajout de la methode affchant un message d'erreur sur le chargement d'un Ilogable

--- Client/API_FTP/FTP_Client/MethodesGlobales.cs
+++ Client/API_FTP/FTP_Client/MethodesGlobales.cs
@@ -9,14 +9,19 @@ namespace API_FTP.FTP_Client
 {
     public static abstract class MethodesGlobales
     {
-        public static void AfficherMessager(string message, string titre)
+        public static void AfficherMessage(string message, string titre)
         {
             MessageBox.Show(message, titre);
         }
 
         public static void AfficherMessageErreurInitialisationILog()
         {
-            AfficherMessager(VariablesGlobales.GetContenuMessageErreurInitialisationILog, VariablesGlobales.GetTitreMessageErreurInitialisationILog);
+            AfficherMessage(VariablesGlobales.GetContenuMessageErreurInitialisationILog, VariablesGlobales.GetTitreMessageErreurInitialisationILog);
+        }
+
+        public static void AfficherMessageErreurInitialisationILogable()
+        {
+            AfficherMessage(VariablesGlobales.GetContenuMessageErreurInitialisationILogable, VariablesGlobales.GetTitreMessageErreurInitialisationILogable);
         }
     }
 }
