9187a224e017359a314e39d19775764bb390e89b
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:50:17 +0100

    Ajout de commentaire à la classe

--- Client/API_FTP/FTP_Client/MethodesGlobales.cs
+++ Client/API_FTP/FTP_Client/MethodesGlobales.cs
@@ -7,18 +7,32 @@ using System.Windows.Forms;
 
 namespace API_FTP.FTP_Client
 {
+    /// <summary>
+    /// Class fournissant l'enesemble des methodes globales du client FTP
+    /// </summary>
     public static abstract class MethodesGlobales
     {
+        /// <summary>
+        /// Affiche un message à l'utilisateur
+        /// </summary>
+        /// <param name="message">le message</param>
+        /// <param name="titre">le titre du message</param>
         public static void AfficherMessage(string message, string titre)
         {
             MessageBox.Show(message, titre);
         }
 
+        /// <summary>
+        /// Affiche un message d'erreur correspondant à une erreur de chargement du ILog
+        /// </summary>
         public static void AfficherMessageErreurInitialisationILog()
         {
             AfficherMessage(VariablesGlobales.GetContenuMessageErreurInitialisationILog, VariablesGlobales.GetTitreMessageErreurInitialisationILog);
         }
 
+        /// <summary>
+        /// Affiche un message d'erreur correspondant à une erreur de chargement du ILogable
+        /// </summary>
         public static void AfficherMessageErreurInitialisationILogable()
         {
             AfficherMessage(VariablesGlobales.GetContenuMessageErreurInitialisationILogable, VariablesGlobales.GetTitreMessageErreurInitialisationILogable);
