72459359dd2b6d07a03294bc23876f7a56aad6c2
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:57:39 +0100

    Mise à jour LogFTP

--- Client/API_FTP/FTP_Client/Classes/LogFtp.cs
+++ Client/API_FTP/FTP_Client/Classes/LogFtp.cs
@@ -8,54 +8,81 @@ using System.Windows.Forms;
 
 namespace API_FTP.FTP_Client.Classes
 {
+    /// <summary>
+    /// Classe définissant la structure du logFTP
+    /// </summary>
     abstract class LogFtp : ILog
     {
-        protected ILogable _monLogueur;
+        /// <summary>
+        /// est l'attribut définissant le ILogable
+        /// </summary>
+        protected ILogable _monLogable;
 
-        public void Log(IStatus leStatut)
+        /// <summary>
+        /// Charge le logable en testant si il n'est pas null. S'il est null il affiche un message à l'utilisateur
+        /// </summary>
+        /// <param name="unLogable">le Ilogable</param>
+        protected void ChargerLogable(ILogable unLogable)
         {
-            if (_monLogueur == null)
+            if (unLogable == null)
             {
-                MethodesGlobales.AfficherMessageErreurInitialisationILog();
+                MethodesGlobales.AfficherMessageErreurInitialisationILogable();
             }
             else
             {
-                _monLogueur.AddLog(leStatut.GetStatusDetail());
+                _monLogable = unLogable;
             }
         }
 
+        /// <summary>
+        /// Envoie un Statut que doit afficher le ILogable sous forme de message
+        /// </summary>
+        /// <param name="leStatut">le statut</param>
+        public void Log(IStatus leStatut)
+        {
+            if (_monLogable == null)
+            {
+                MethodesGlobales.AfficherMessageErreurInitialisationILogable();
+            }
+            else
+            {
+                _monLogable.AddLog(leStatut.GetStatusDetail());
+            }
+        }
+
+        /// <summary>
+        /// Envoie un message personnalisé que doit afficher le ILogable
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <param name="estUneCommande">définit si c'est une commande ou une réponse. True pour commande</param>
         public void LogCustom(string message, bool estUneCommande = false)
         {
-            if (_monLogueur == null)
+            if (_monLogable == null)
             {
-                MethodesGlobales.AfficherMessageErreurInitialisationILog();
+                MethodesGlobales.AfficherMessageErreurInitialisationILogable();
             }
             else
             {
                 if (estUneCommande)
                 {
                     StatusCommand laCommande = new StatusCommand(message);
-                    _monLogueur.AddLogCustom(laCommande.GetStatusDetail());
+                    _monLogable.AddLogCustom(laCommande.GetStatusDetail());
                 }
                 else
                 {
                     StatusResponse laReponse = new StatusResponse(message);
-                    _monLogueur.AddLogCustom(laReponse.GetStatusDetail());
+                    _monLogable.AddLogCustom(laReponse.GetStatusDetail());
                 }
             }
         }
 
-
+        /// <summary>
+        /// Définit le Ilogable à utiliser
+        /// </summary>
+        /// <param name="lElementLogable">le ILogable</param>
         public void DefinirElementLogable(ILogable lElementLogable)
         {
-            if (lElementLogable == null)
-            {
-                MethodesGlobales.AfficherMessageErreurInitialisationILog();
-            }
-            else
-            {
-                _monLogueur = lElementLogable;
-            }
+            ChargerLogable(lElementLogable);
         }
     }
 }
