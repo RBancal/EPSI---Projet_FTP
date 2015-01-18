9fbfa152ab97d78e3072f61cfcc5a4de796a9c9c
myc1986 <moratinos.c@gmail.com>
2015-01-18 13:15:46 +0100

    changement d parametre Addlog en string

--- Client/API_FTP/FTP_Client/Interfaces/ILogable.cs
+++ Client/API_FTP/FTP_Client/Interfaces/ILogable.cs
@@ -14,13 +14,13 @@ namespace API_FTP.FTP_Client.Interfaces
         /// <summary>
         /// Ajout une message de log à l'élément
         /// </summary>
-        /// <param name="leStatut">le statut à logguer</param>
-        void AddLog(IStatus leStatut);
+        void AddLog(string message);
 
         /// <summary>
         /// Ajout du message personnalisé de log à l'élément
         /// </summary>
         /// <param name="message">le statut à logguer</param>
-        void AddLogCustom(string message);
+        /// <param name="estUneCommande">définit si c'est une commande personnalisée ou une réponse. True pour une commande et false pour pour réponse. Par défaut c'est une réponse.</param>
+        void AddLogCustom(string message, bool estUneCommande = false);
     }
 }
