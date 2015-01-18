ce7d411912dcfe07647fb608a60476322644e4b6
myc1986 <moratinos.c@gmail.com>
2015-01-18 16:02:16 +0100

    mise à jour logFTP

--- Client/API_FTP/FTP_Client/Classes/LogFtp.cs
+++ Client/API_FTP/FTP_Client/Classes/LogFtp.cs
@@ -1,21 +1,64 @@
-﻿using System;
+﻿using API_FTP.FTP_Client.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace API_FTP.FTP_Client.Classes
 {
-    class LogFtp
+    abstract class LogFtp : ILog
     {
-        public static void log()
+        protected ILogable _monLogueur;
+
+        protected const string ERREUR_INITIALISATION_MESSAGE = "Ce logueur n'est pas initialisé. Merci de l'initialisé";
+        protected const string ERREUR_INITIALISATION_TITRE = "Erreur - Logueur";
+
+        public void Log(IStatus leStatut)
         {
-            lL
+            if (_monLogueur == null)
+            {
+                MessageBox.Show(ERREUR_INITIALISATION_MESSAGE, ERREUR_INITIALISATION_TITRE);
+            }
+            else
+            {
+                _monLogueur.AddLog(leStatut.GetStatusDetail());
+            }
         }
 
-        public static void logCustom()
+        public void LogCustom(string message, bool estUneCommande = false)
         {
+            if (_monLogueur == null)
+            {
+                MessageBox.Show(ERREUR_INITIALISATION_MESSAGE, ERREUR_INITIALISATION_TITRE);
+            }
+            else
+            {
+                if (estUneCommande)
+                {
+                    StatusCommand laCommande = new StatusCommand(message);
+                    _monLogueur.AddLogCustom(laCommande.GetStatusDetail());
+                }
+                else
+                {
+                    StatusResponse laReponse = new StatusResponse(message);
+                    _monLogueur.AddLogCustom(laReponse.GetStatusDetail());
+                }
+            }
+        }
 
+
+        public void DefinirElementLogable(ILogable lElementLogable)
+        {
+            if (lElementLogable == null)
+            {
+                MessageBox.Show(ERREUR_INITIALISATION_MESSAGE, ERREUR_INITIALISATION_TITRE);
+            }
+            else
+            {
+                _monLogueur = lElementLogable;
+            }
         }
     }
 }
