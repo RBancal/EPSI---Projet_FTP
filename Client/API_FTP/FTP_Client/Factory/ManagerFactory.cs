b68927171d5e4f07dc28aed3b981ab6c3f94bcdc
myc1986 <moratinos.c@gmail.com>
2015-01-19 09:49:12 +0100

    ajout ManagerFactory

---
+++ Client/API_FTP/FTP_Client/Factory/ManagerFactory.cs
@@ -0,0 +1,33 @@
+﻿using API_FTP.FTP_Client.Classes;
+using API_FTP.FTP_Client.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_FTP.FTP_Client.Factory
+{
+    static class ManagerFactory
+    {
+        public static Manager Fabriquer(string gestionnaire, ILogable leLogable, Configuration laConfig = null)
+        {
+            Manager unManager = null;
+            LogFtp leLogFtp = new LogFtp();
+            leLogFtp.DefinirElementLogable(leLogable);
+
+
+            if (gestionnaire == "$LocalManager")
+            {
+                
+                unManager = new LocalManager(new ElementFolder(@"c:\"), leLogFtp);
+            }
+            else
+            {
+                unManager = new DistantManager(laConfig, leLogFtp);
+            }
+
+            return unManager;
+        }
+    }
+}
