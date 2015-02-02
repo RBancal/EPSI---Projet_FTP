b9501fee27295493187c311793425c70de6e07b8
myc1986 <moratinos.c@gmail.com>
2015-02-02 15:40:59 +0100

    Ajout d'un control d'affichage des logs

---
+++ Client/API_FTP/FTP_Client/Controls/ListBoxLogFtp.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace API_FTP.FTP_Client.Controls
+{
+    partial class ListBoxLogFtp
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_messagesLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lst_messagesLog
+            // 
+            this.lst_messagesLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_messagesLog.FormattingEnabled = true;
+            this.lst_messagesLog.Location = new System.Drawing.Point(0, 0);
+            this.lst_messagesLog.Name = "lst_messagesLog";
+            this.lst_messagesLog.Size = new System.Drawing.Size(321, 162);
+            this.lst_messagesLog.TabIndex = 0;
+            // 
+            // PanelLogFtp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lst_messagesLog);
+            this.Name = "PanelLogFtp";
+            this.Size = new System.Drawing.Size(321, 162);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_messagesLog;
+    }
+}
---
+++ Client/API_FTP/FTP_Client/Controls/ListBoxLogFtp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using API_FTP.FTP_Client.Interfaces;
+
+namespace API_FTP.FTP_Client.Controls
+{
+    public partial class ListBoxLogFtp : UserControl, ILogable
+    {
+        public ListBoxLogFtp()
+        {
+            InitializeComponent();
+        }
+
+        public void AddLog(string message)
+        {
+            lst_messagesLog.Items.Add(message);
+        }
+
+        public void AddLogCustom(string message, bool estUneCommande = false)
+        {
+            lst_messagesLog.Items.Add(message);
+        }
+    }
+}
